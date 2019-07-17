0201c9bcf7aada4d9587d0366b8b565fb72befec
kenny <kenny2009108@gmail.com>
2019-07-17 09:55:40 +0800

    init

--- src/frontend/components/Home.js
+++ src/frontend/components/Home.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import { Typography, Container, Button, Divider } from '@material-ui/core';
 import { MAIN_ROUTE } from "../../shared/constants/routes";
+import ReactNotifications from 'react-browser-notifications';
 
 class Home extends React.Component {
     constructor(props) {
         super(props);
+        this.showNotifications = this.showNotifications.bind(this);
+        this.handleClick = this.handleClick.bind(this);
+    }
+    showNotifications() {
+        if(this.n.supported()){
+            this.n.show();
+        }else{
+            alert(JSON.stringify(this.n))
+        }
+    }
+    handleClick(event) {
+        this.n.close(event.target.tag);
     }
     content(type) {
         switch(type){
@@ -27,6 +40,16 @@ class Home extends React.Component {
                 <Typography variant="h7" gutterBottom>The value of variable type is {this.props.home.type}</Typography>
                 <Divider />
                 <Button onClick={() => this.props.history.push(MAIN_ROUTE)}>Click to switch pages</Button>
+                <ReactNotifications
+                    onRef={ref => (this.n = ref)} // Required
+                    title="Hey There!" // Required
+                    body="This is the body"
+                    timeout="2000"
+                    onClick={event => this.handleClick(event)}
+                />
+                <button onClick={this.showNotifications}>
+                    Notify Me!
+                </button>
             </Container>
         )
     }
